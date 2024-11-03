274beaeeca3919d7275ba0fe912baa478ff54faf
Peiprjs <m.rocacugat@student.maastrichtuniversity.nl>
2024-11-03 04:11:27 +0100

    Fixes

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 yesterday="$(($(date +'%d')-1))-$(date +'%m-%Y')"
 echo "Moved yesterday's puzzle to $yesterday"
-mv today "archive/$yesterday"
-echo "Downloading today's"
+mv today "archive/$yesterday" -f
+#------------------------------------#
+echo "Downloading today's puzzle"
 wget "https://raw.githubusercontent.com/Peiprjs/DailyTuring/refs/heads/main/today"
 echo "Today has been downloaded successfully"
