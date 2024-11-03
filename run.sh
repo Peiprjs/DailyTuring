dc33e17eb6c4d3f3e2654668c9f3fa233bed9e3b
Peiprjs <m.rocacugat@student.maastrichtuniversity.nl>
2024-11-03 22:59:45 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,11 +4,21 @@ mv today "archive/$yesterday" -f
 mkdir today
 #------------------------------------#
 echo "Downloading today's criteria"
-wget "https://raw.githubusercontent.com/Peiprjs/DailyTuring/refs/heads/main/today/criteria" -P "today"
+wget "https://raw.githubusercontent.com/Peiprjs/DailyTuring/refs/heads/main/today/criteria" -bqP "today"
 echo "Today's criteria has been downloaded successfully"
+#------------------------------------#
 echo "Downloading today's verifiers"
-wget "https://raw.githubusercontent.com/Peiprjs/DailyTuring/refs/heads/main/today/verifiers" -P "today"
+wget "https://raw.githubusercontent.com/Peiprjs/DailyTuring/refs/heads/main/today/verifiers" -bqP "today"
 echo "Today's verifiers has been downloaded successfully"
+#------------------------------------#
 echo "Downloading today's solution"
-wget "https://raw.githubusercontent.com/Peiprjs/DailyTuring/refs/heads/main/today/solution" -P "today"
+wget "https://raw.githubusercontent.com/Peiprjs/DailyTuring/refs/heads/main/today/solution" -bqP "today"
 echo "Today's solution has been downloaded successfully"
+#------------------------------------#
+echo "Downloading today's hash"
+wget "https://raw.githubusercontent.com/Peiprjs/DailyTuring/refs/heads/main/today/hash" -bqP "today"
+echo "Today's hash has been downloaded successfully"
+#------------------------------------#
+echo "Downloading today's score"
+wget "https://raw.githubusercontent.com/Peiprjs/DailyTuring/refs/heads/main/today/score" -bqP "today"
+echo "Today's score has been downloaded successfully"
