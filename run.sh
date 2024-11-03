3fdf41499e06ac52a9fe000f59a213be1ed0f083
Peipr <m.rocacugat@student.maastrichtuniversity.nl>
2024-11-03 02:50:14 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 yesterday="$(($(date +'%d')-1))-$(date +'%m-%Y')"
 echo "Moved yesterday's puzzle to $yesterday"
-mv daily "archive/$yesterday"
+mv today "archive/$yesterday"
 echo "Running Main"
 node main.js> today
 echo "Main run successfully"
