ba544594afd0a5344b3bbb3fbc6f6632c8afee65
Peiprjs <m.rocacugat@student.maastrichtuniversity.nl>
2024-11-03 02:20:12 +0100

    Why?? Why?? WHY??? Oh...that's why...I made a stupid mistake....

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 yesterday="$(($(date +'%d')-1))-$(date +'%m-%Y')"
 echo "Moved yesterday's puzzle to $yesterday"
-mv daily "archive/$(date +'%F')"
+mv daily "archive/$yesterday"
 echo "Running Main"
 node main.js> daily
 echo "Main run successfully"
