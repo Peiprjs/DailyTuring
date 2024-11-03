6f575be711bab24a226a942b8957e7e54beda6d0
Peipr <m.rocacugat@student.maastrichtuniversity.nl>
2024-11-03 04:07:07 +0100

    Working out the kinks

--- run.sh
+++ run.sh
@@ -3,11 +3,11 @@ mv today "archive/$yesterday" -f
 echo "Moved yesterday's puzzle to $yesterday"
 #-------------------------------------#
 echo "Getting Criteria"
-node criteria.js> today/criteria
+node scrapers/criteria.js> today/criteria
 echo "Criteria run successfully"
 echo "Getting Solution"
-node solution.js> today/solution
+node scrapers/solution.js> today/solution
 echo "Solution run successfully"
 echo "Getting Verifiers"
-node verifiers.js> today/verifiers
+node scrapers/verifiers.js> today/verifiers
 echo "Verifiers run successfully"
